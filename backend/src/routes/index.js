73a16d8f66b89908a1af367eaef73c4021a155bd
awahids <awahid.safhadi@ethisindonesia.com>
2025-01-19 19:04:27 +0700

    Update package.json to specify main entry point and modify dev script; remove unused welcome route in index.js

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -1,13 +1,8 @@
 var express = require('express');
 var router = express.Router();
-const { sendSuccessResponse } = require('../helpers/responses.helper');
 const raksRouter = require('./rak.routes');
 const booksRouter = require('./book.routes');
 const studentsRouter = require('./student.routes');
-
-router.get('/', function (req, res, next) {
-  return sendSuccessResponse(res, Date('YYYY-MM-DD'), 'Welcome to the API', 200);
-});
 
 router.use('/', raksRouter);
 router.use('/', booksRouter);
